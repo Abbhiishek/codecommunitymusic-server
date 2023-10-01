fc566d5c34308b6f18078b64f371b70abb206e48
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-10-01 09:23:45 +0530

    fix: gmail-support
    Signed-off-by: Abhishek kushwaha <abhishekkushwaha1479@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -174,7 +174,10 @@ MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'codecommunitymusic@gmail.com',
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD"),
+EMAIL_HOST_USER = 'codecommunitymusic@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
+DEFAULT_FROM_EMAIL='codecommunitymusic@gmail.com'
